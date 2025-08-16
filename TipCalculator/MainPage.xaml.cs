db545830bcc564a65f798b9b1032a9fe821c1ae1
Vishal Pawar <vishal.vicky.pawar@gmail.com>
2025-08-16 20:29:46 +0530

    tip calculator application completed

--- TipCalculator/MainPage.xaml.cs
+++ TipCalculator/MainPage.xaml.cs
@@ -2,23 +2,62 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        decimal billAmount = 0.00m;
+        int tip = 0;
+        decimal tipAmount = 0.00m;
+        int split = 1;
+        decimal amountPerPerson = 0.00m;
+
+        private void UpdateDisplay()
+        {
+            lblTip.Text = tip.ToString();
+            lblTipAmount.Text = tipAmount.ToString("F2");
+            lblSplit.Text = split.ToString();
+            lblSplitAmount.Text = amountPerPerson.ToString("F2");
+        }
 
         public MainPage()
         {
             InitializeComponent();
+            UpdateDisplay();
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private void tbTotal_Completed(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                billAmount = decimal.Parse(tbTotal.Text);
+                Calculate();
+            } catch(Exception) { }
+            tbTotal.Unfocus();
+            slrTip.Focus();
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        private void Calculate()
+        {
+            tipAmount = billAmount * tip / 100;
+            amountPerPerson = (billAmount + tipAmount) / split;
+            UpdateDisplay();
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void slrTip_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            tip = (int)Math.Round(slrTip.Value);
+            slrTip.Value = tip;
+            Calculate();
+        }
+
+        private void btnMinus_Clicked(object sender, EventArgs e)
+        {
+            if (split > 1)
+                split--;
+            Calculate();
+        }
+
+        private void brnPlus_Clicked(object sender, EventArgs e)
+        {
+            split++;
+            Calculate();
         }
     }
 }
